a35c344ab1b5c34cfcea7c8a550c899e09a7e45e
lilacflower13 <lilavail@seattleacademy.org>
2025-01-14 18:51:03 +0000

    project 3

---
+++ Projects/Project3.py
@@ -0,0 +1,24 @@
+### setup
+import turtle
+turtle.Screen().bgcolor("black")
+t1 = turtle.Turtle()
+t = turtle.Turtle()
+###Pink turtle
+t.goto(-50,-50)
+t.color("pink")
+t.speed(10)
+for i in range ( 600 ):
+    t.forward(100+i)
+    t.left(90+1)
+###second turtle
+t1.speed(10)
+t1.penup()
+t1.goto(0,0)
+t1.pendown()
+t1.color("light blue")
+for i in range ( 450 ):
+    t1.forward(1+i)
+    t1.left(120+1)
+
+
+turtle.exitonclick()
